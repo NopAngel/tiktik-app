2b48ae5d75eda518d0076c30e813dbbb4be3b5b0
NoAngel <155915830+Angel45003@users.noreply.github.com>
2024-06-15 18:39:02 -0400

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default async function Home() {
         <Navbar />
         <section className="lg:w-[450px] sm:w-full mx-auto mt-9">
           <FormDB />
-          {db1?.map((item: DataProps, index => (
+          {db1?.map((item, index => (
             <Card
               ID={index}
               Content={item.content}
