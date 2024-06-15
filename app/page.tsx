cdedfe26912338b7186654fecbd6db00f3adbaa8
NoAngel <155915830+Angel45003@users.noreply.github.com>
2024-06-15 18:36:31 -0400

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -19,6 +19,7 @@ export default async function Home() {
           <FormDB />
           {db1?.map((item: DataProps, index => (
             <Card
+              ID={index}
               Content={item.content}
               CreatedAt={item.created_at}
               UserImg={item.user_img}
