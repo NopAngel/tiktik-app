2ef2bc9e6aa9e9b31ea4ad8c6cc8458e9aebafaf
NoAngel <155915830+Angel45003@users.noreply.github.com>
2024-06-15 19:53:58 -0400

    Card Element🚀

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,22 +1,14 @@
 export default function Card({
-  ID,
-  CreatedAt,
   Content,
   UserName,
   UserImg,
 }: {
-  ID: any;
-  CreatedAt: any;
   Content: any;
   UserName: any;
   UserImg: any;
 }) {
-  console.log(ID);
   return (
-    <section
-      key={ID}
-      className="border-[1px] border-neutral-300/55 cursor-pointer hover:bg-neutral-100 transition-all duration-100 lg:w-[450px] sm:w-full sm:text-center rounded-lg mt-6"
-    >
+    <section className="border-[1px] border-neutral-300/55 cursor-pointer hover:bg-neutral-100 transition-all duration-100 lg:w-[450px] sm:w-full sm:text-center rounded-lg mt-6">
       <header className="border-b-[1px] border-neutral-300/55 pb-3">
         <h1 className="flex items-center justify-center gap-4">
           <img src={UserImg} alt={UserName} className="w-10 rounded-full" />
